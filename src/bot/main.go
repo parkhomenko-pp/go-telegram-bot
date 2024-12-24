b03974a6fcaf11ec71073a50c7e5d0c4ed5423c8
Dmitry Parkhomenko <parkhomenko@perfectpanel.com>
2024-12-24 18:04:48 +0300

    add echo bot

--- src/bot/main.go
+++ src/bot/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	apiKey := os.Getenv("TELEGRAM_API_KEY")
+	if apiKey == "" {
+		log.Fatalf("TELEGRAM_API_KEY not set in .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(apiKey)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			// log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
